48c4ac6763968b25cce37f936a52b38167f40739
nn <205666415+guessmesome@users.noreply.github.com>
2025-11-12 12:11:14 +0700

    upd

---
+++ config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+    products: {
+        1: 'https://vldmnt.ru/go/spmixmgwmv',
+        2: 'https://vldmnt.ru/go/srmqo7nhac',
+        3: 'https://vldmnt.ru/go/sl7jzlt5ec',
+        4: 'https://vldmnt.ru/go/slsu83i3oy'
+    }
+};
+
