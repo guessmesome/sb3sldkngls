40423acf99869c82bdceebec58366cd555960ce1
nn <205666415+guessmesome@users.noreply.github.com>
2025-11-16 12:26:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     products: {
-        1: 'https://vldmnt.ru/go/spmixmgwmv',
+        1: 'https://vldmnt.ru/go/s0masiklat',
         2: 'https://vldmnt.ru/go/srmqo7nhac',
         3: 'https://vldmnt.ru/go/slsu83i3oy',
         4: 'https://vldmnt.ru/go/sl7jzlt5ec'
